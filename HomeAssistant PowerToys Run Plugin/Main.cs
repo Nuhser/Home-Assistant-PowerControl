18f5bbc46346b304e46754dfea26cd0fdbda4d0a
Nuhser <martinimmel@gmail.com>
2023-02-23 09:33:07 +0100

    First commit

---
+++ HomeAssistant PowerToys Run Plugin/Main.cs
@@ -0,0 +1,13 @@
+﻿using Wox.Plugin;
+
+namespace HomeAssistant_PowerToys_Run_Plugin;
+
+public class Main : IPlugin {
+    public void Init(PluginInitContext context) {
+        throw new NotImplementedException();
+    }
+    
+    public List<Result> Query(Query query) {
+        throw new NotImplementedException();
+    }
+}
